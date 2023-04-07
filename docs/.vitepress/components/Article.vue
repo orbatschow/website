bdcc504d523bdc84c515f831cbd7a1ab49698b8a
nilsorbat <nils@orbat.sh>
2023-04-07 13:17:20 +0200

    fix links on direct page loading

--- docs/.vitepress/components/Article.vue
+++ docs/.vitepress/components/Article.vue
@@ -1,8 +1,9 @@
 <script setup>
 
-import { useRoute } from 'vitepress'
-const route = useRoute()
+import {useRoute} from 'vitepress'
+import {data as articles} from "./articles.data";
 
+const route = useRoute()
 </script>
 
 
@@ -10,13 +11,14 @@ const route = useRoute()
   <Content class="vp-doc mb-auto mt-6"/>
 
   <!-- this section will only be rendered for article pages -->
-  <section v-if="route.path.includes('/blog/')" class="flex mt-6 flex-col md:flex-row gap-y-8 md:gap-x-10 justify-between border-t pt-8 border-t-divider">
+  <section v-if="route.path.includes('/blog/')"
+           class="flex mt-6 flex-col md:flex-row gap-y-8 md:gap-x-10 justify-between border-t pt-8 border-t-divider">
     <a class="flex md:w-1/2 text-secondary hover:opacity-70 transition p-4 border border-secondary" target="_blank"
-       :href="'https://github.com/orbatschow/website/blob/master/docs' + route.path + 'index.md'">
+       :href="'https://github.com/orbatschow/website/blob/master/docs' + route.path.replace('index', '') + 'index.md'">
       Edit this page on Github
     </a>
     <a class="flex md:w-1/2 text-right text-secondary hover:opacity-70 transition p-4 border border-secondary"
-       target="_blank" :href="'https://github.com/orbatschow/website/blob/master/docs' + route.path + 'code'">
+       target="_blank" :href="'https://github.com/orbatschow/website/blob/master/docs' + route.path.replace('index', '') + 'code'">
           <span class="block w-full text-right">
             Find the source code on GitHub
           </span>
