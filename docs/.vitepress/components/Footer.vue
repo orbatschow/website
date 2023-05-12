1a21463d19d2fdb4f5ebb4fc3728e1a9db7c9eb5
nilsorbat <nils@orbat.sh>
2023-05-12 17:07:03 +0200

    rework footer

--- docs/.vitepress/components/Footer.vue
+++ docs/.vitepress/components/Footer.vue
@@ -1,22 +1,14 @@
 <template>
 
-  <!-- smartphone -->
-  <footer class="flex flex-row my-9 justify-between font-semibold text-lg flex sm:hidden">
-    <div>
-      <span class="block mb-2">© {{ new Date().getFullYear() }}</span>
-      <span class="bl">Nils Orbat</span>
+  <footer class="flex flex-row my-9 justify-between font-semibold text-lg flex">
+    <div class="flex flex-col sm:flex-row">
+      <span class="mb-2">© {{ new Date().getFullYear() }}</span>
+      <span class="hidden sm:inline">&nbsp;•&nbsp;</span>
+      <span>Nils Orbat</span>
     </div>
-    <div class="flex flex-col text-right">
+    <div class="flex flex-col sm:flex-row text-right">
       <a class="hover:text-secondary mb-2" href="/legal/">Legal</a>
-      <a class="hover:text-secondary" href="/privacy-policy/">Privacy Policy</a>
-    </div>
-  </footer>
-
-  <!-- tablet and bigger -->
-  <footer class="flex flex-row my-9 justify-between font-semibold text-lg hidden sm:flex">
-    <span>© {{ new Date().getFullYear() }} • Nils Orbat</span>
-    <div>
-      <a class="hover:text-secondary" href="/legal/">Legal</a> •
+      <span class="hidden sm:inline">&nbsp;•&nbsp;</span>
       <a class="hover:text-secondary" href="/privacy-policy/">Privacy Policy</a>
     </div>
   </footer>
