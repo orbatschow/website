1f75ecb5c435ad70139adb3fd459ff079682c76c
orbatschow <nils@orbat.sh>
2026-03-13 19:29:18 +0100

    feat: change header title

--- docs/.vitepress/components/Header.vue
+++ docs/.vitepress/components/Header.vue
@@ -1,5 +1,4 @@
 <style scoped>
-
 button.negative-margin {
   margin-right: -15px;
 }
@@ -11,7 +10,6 @@ button.negative-margin {
 .hamburger.is-active:hover {
   opacity: 1 !important;
 }
-
 </style>
 
 <script>
@@ -42,21 +40,20 @@ export default {
 <template>
   <header class="flex flex-row my-6 md:my-9 items-center relative text-lg font-semibold">
     <a href="/" v-on:click="closeMobileMenu" class="mr-auto whitespace-nowrap hover:text-secondary">
-      nils@orbat.sh:~$ <span class="text-secondary">_</span>
+      root@orbat.sh:~$ <span class="text-secondary">_</span>
     </a>
 
     <nav class="flex self-end">
       <!-- mobile header (hamburger) -->
       <!-- https://github.com/jonsuh/hamburgers/issues/66#issuecomment-491905089 --->
       <button ref="hamburger" v-on:click="toggleMobileMenu"
-              class="sm:!hidden negative-margin hamburger hamburger--3dxy !leading-none"
-              type="button">
+        class="sm:!hidden negative-margin hamburger hamburger--3dxy !leading-none" type="button">
         <span class="hamburger-box !w-8 !block">
           <span class="hamburger-inner !w-8 before:!w-8 after:!w-8 !bg-font after:!bg-font before:!bg-font"></span>
         </span>
       </button>
 
-    <!-- tablet and larger header -->
+      <!-- tablet and larger header -->
       <ul class="hidden sm:flex text-end gap-x-8">
         <li>
           <a class="hover:text-secondary" v-on:click="closeMobileMenu" href="/about/">about</a>
@@ -69,7 +66,7 @@ export default {
 
     <!-- mobile menu -->
     <section ref="mobile-menu"
-             class="!bg-primary flex flex-col absolute left-0 right-0 top-20 hidden transition-transform z-10">
+      class="!bg-primary flex flex-col absolute left-0 right-0 top-20 hidden transition-transform z-10">
       <ul class="flex flex-col text-end border-t">
         <a class="hover:text-secondary" v-on:click="toggleMobileMenu" href="/about/">
           <li class="ml-auto w-full py-6  border-b">
